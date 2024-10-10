3222a7b8939d27690c2d528e99ad650059e40b1d
Fabian <fabianjoseph063@gmail.com>
2024-10-10 15:23:24 +0100

    Update server.js
    Found a way to upload .env file on vercel

--- server.js
+++ server.js
@@ -22,7 +22,8 @@ async function appendToGoogleSheet(data) {
   
 const auth = new google.auth.GoogleAuth({
   credentials: {
-    private_key: Buffer.from(process.env.GOOGLE_PRIVATE_KEY_BASE64, 'base64').toString('utf-8'),    client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    private_key: process.env.GOOGLE_PRIVATE_KEY_BASE64,
+    client_email: process.env.GOOGLE_CLIENT_EMAIL,
   },
   projectId: process.env.GOOGLE_PROJECT_ID,
   scopes: ['https://www.googleapis.com/auth/spreadsheets'],
