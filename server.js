64d37051010d8a80e85f991f86982d07c0259b07
Fector101 <fabianjoseph063@gmail.com>
2024-10-10 18:57:05 +0100

    alas a secrets are secrets

--- server.js
+++ server.js
@@ -20,7 +20,21 @@ async function appendToGoogleSheet(data) {
 
   // Inserting credentials.json Into Parser
   const auth = new google.auth.GoogleAuth({
-    keyFile: path.join(__dirname,'credentials.json'),
+    // keyFile: path.join(__dirname,'credentials.json'),
+      credentials:{ 
+                    type: process.env.type,
+                    project_id: process.env.project_id,
+                    private_key_id: process.env.private_key_id,
+                    private_key: process.env.private_key,
+                    client_email: process.env.client_email,
+                    client_id: process.env.client_id,
+                    auth_uri: process.env.auth_uri,
+                    token_uri: process.env.token_uri,
+                    auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+                    client_x509_cert_url: process.env.client_x509_cert_url,
+                    universe_domain: process.env.universe_domain,
+                    sheet_id: process.env.sheet_id
+    },
     scopes: ['https://www.googleapis.com/auth/spreadsheets'],
   })
 
